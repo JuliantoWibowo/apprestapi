ea23bfc41aae64fadde08adfbb16493c03dd2ba3
Julianto <juliwibowo@outlook.com>
2020-03-29 20:17:16 +0700

    Menambahkan kode res

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-
 //parse.application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
