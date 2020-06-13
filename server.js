3d60f5e42cafe68813409ed20ed4c883d5b86992
Julianto <juliwibowo@outlook.com>
2020-06-13 21:02:03 +0700

    Install Cors

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
+var cors = require('cors');
 
 //parse.application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cors());
 
 //panggil routes
 var routes =require("./routes");
 routes(app);
 
-app.listen(3000, () => {
-    console.log(`Server started on port`);
+app.listen(3001, () => {
+    console.log(`Server started on port 3001`);
 });
